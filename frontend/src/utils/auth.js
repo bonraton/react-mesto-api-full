c44158b59643e124623a3229d64d0032644957e6
bonraton <bonraton@yandex.ru>
2021-11-02 15:05:23 +0300

    CORS

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://api.nomoredomains.mesto.nomoredomains.work";
+export const BASE_URL = "https://api.nomoredomains.mesto.nomoredomains.work/signup";
 
 export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
