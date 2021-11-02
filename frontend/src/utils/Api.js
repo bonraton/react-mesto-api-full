0a9ac8dceed090ccf174d26dc4e869f61d0a7e83
bonraton <bonraton@yandex.ru>
2021-11-02 15:44:02 +0300

    CORS

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,6 +1,6 @@
 export class Api {
   constructor() {
-    this._adress = 'https://api.nomoredomains.mesto.nomoredomains.work';
+    this._adress = 'api.nomoredomains.mesto.nomoredomains.work';
     this._token = ` Bearer ${localStorage.jwt}`;
   }
 
