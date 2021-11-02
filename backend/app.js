4ff3e5d0aec34a45deec7c8010ed92b88da047bf
bonraton <bonraton@yandex.ru>
2021-11-02 16:47:18 +0300

    CORS

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(cors());
-// app.options('*', cors())
+app.options('*', cors())
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
