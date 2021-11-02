8643e3dcb2230f5823522f14f92c33b6507498e6
bonraton <bonraton@yandex.ru>
2021-11-02 20:43:26 +0300

    CORS

--- backend/app.js
+++ backend/app.js
@@ -28,20 +28,7 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions));
-app.options('*', cors());
-app.use(function (req, res, next) {
-  if (req.method === 'OPTIONS') {
-    res.sendStatus('200');
-    next();
-  }
-})
-
-// app.use(cors());
-// app.options('*', cors(), function (req,res) {
-//   if (req.method === 'OPTIONS') {
-//     res.sendStatus(200);
-//   }
-// });
+app.options('*', cors(corsOptions));
 
 
 // app.use(cors(corsOptions));
