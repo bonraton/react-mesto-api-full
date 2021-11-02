0f1d8a4b279c9f91b23110c1d620c798458f29eb
bonraton <bonraton@yandex.ru>
2021-11-02 17:48:31 +0300

    CORS

--- backend/app.js
+++ backend/app.js
@@ -7,32 +7,31 @@ const { registerValidator, loginValidator } = require('./middlewares/validation'
 const cors  = require('cors');
 
 
-// const allowedCors = ['http://localhost:3000',
-//                      'https://nomoredomains.mesto.nomoredomains.rocks',
-//                      'http://nomoredomains.mesto.nomoredomains.rocks',
-//                      'https://api.nomoredomains.mesto.nomoredomains.work',
-//                      'http://api.nomoredomains.mesto.nomoredomains.work',
-//                      'http://nomoredomains.mesto.nomoredomains.rocks/signup',
-//                      'http://nomoredomains.mesto.nomoredomains.rocks/users/me'
-                    // ]
-// const DEFAULT_ALLOWEDMETHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
+const allowedCors = ['http://localhost:3000',
+                     'https://nomoredomains.mesto.nomoredomains.rocks',
+                     'http://nomoredomains.mesto.nomoredomains.rocks',
+                     'https://api.nomoredomains.mesto.nomoredomains.work',
+                     'http://api.nomoredomains.mesto.nomoredomains.work',
+                    ]
+const DEFAULT_ALLOWEDMETHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
 
-// const corsOptions = {
-//   origin: '*',
-//   methods: DEFAULT_ALLOWEDMETHODS,
-//   allowedHeaders: ['content-type', 'authorization']
-// }
+const corsOptions = {
+  credentials: true,
+  origin: function checkCors(origin, callback) {
+    if (allowedCors.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS HATE'))
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(cors());
-// app.options('*', cors(), function(req, res) {
-//   res.sendStatus(200);
-// })
-app.options('*', function (req, res) {
-  res.sendStatus(200);
-})
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -40,29 +39,6 @@ app.use(express.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewurlParser: true,
 });
-
-
-
-
-// app.use(cors(corsOptions), function(req, res, next) {
-//   const { origin } = req.headers;
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//   }
-//   next();
-// })
-
-// app.use(cors(corsOptions), function(req, res, next) {
-//   const { method } = req;
-//   const requestHeaders  = req.headers['access-control-request-headers'];
-//   if (method === 'OPTIONS') {
-//     res.status(200);
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWEDMETHODS);
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     return res.end();
-//   }
-//   next();
-// })
 
 app.use('/users', jwtCheck, require('./routes/user'));
 app.use('/cards', jwtCheck, require('./routes/cards'));
